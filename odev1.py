6750356103ebedeedf580a5b9320e63e44b32087
alperencucen <76479420+alperencucen@users.noreply.github.com>
2020-12-22 17:44:53 +0300

    Add files via upload

---
+++ odev1.py
@@ -0,0 +1,7 @@
+deger1 = int(input("1. Degeri giriniz(tam sayı):"))
+deger2 = str(input("2. Degeri giriiniz(harf giriniz):"))
+deger3 = float(input("3. degeri giriniz(ondalıklı sayı giriniz):"))
+deger4 = input("4. degeri giriniz:")
+print("1.deger :{}\n2.deger:{}\n3.deger:{}\n4.deger:{}".format(deger1,deger2,deger3,deger4))
+print(type(deger1),type(deger2),type(deger3),type(deger4))
+
